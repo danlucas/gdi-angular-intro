32ebd5018481e06959a28412c7557f9a0bc6f114
Dan Lucas <dan.lucas@gmail.com>
2014-09-26 14:11:55 -0400

    slide app update: directives

--- store/fullapp/scripts/app.js
+++ store/fullapp/scripts/app.js
@@ -1,20 +1,23 @@
 (function(){
-var app = angular.module('collection', [ ]);
+    var app = angular.module('collection', [ ]);
 
-app.controller('UserController', function($scope){
-    $scope.user = user;
-});
-app.controller('StoreController', function(){
-    this.product = movie;
-});
+    app.controller('UserController', function($scope){
+        $scope.user = user;
+    });
+    app.controller('CollectionController', function(){
+        this.item = movie;
+    });
 
-var user = {
-    name: 'Dan Lucas',
-    status: 'primary'
-};
-var movie = {
-    name: "Avengers",
-    price: 19.99,
-    description: 'Superheros destroy city while fighthing a Norse god and his alien friends'
-};
+    var user = {
+        name: 'Dan Lucas',
+        status: 'primary'
+    };
+    var movie = {
+        name: "Marvel's The Avengers",
+        gross: 1518.6,
+        release: new Date('2012-05-04'),
+        inCollection: true,
+        shortDescription: 'Superheros destroy city while fighthing a Norse god and some aliens',
+        longDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    };
 })();
