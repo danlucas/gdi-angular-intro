cbead814d30388dab9d457d8345d6ea72b138458
Dan Lucas <dan.lucas@gmail.com>
2014-09-25 22:08:48 -0400

    app and slide update

--- store/fullapp/scripts/app.js
+++ store/fullapp/scripts/app.js
@@ -1,1 +1,20 @@
+(function(){
 var app = angular.module('store', [ ]);
+
+app.controller('UserController', function($scope){
+    $scope.user = user;
+});
+app.controller('StoreController', function(){
+    this.product = movie;
+});
+
+var user = {
+    name: 'Dan Lucas',
+    status: 'primary'
+};
+var movie = {
+    name: "Avengers",
+    price: 19.99,
+    description: 'Superheros destroy city while fighthing a Norse god and his alien friends'
+};
+})();
